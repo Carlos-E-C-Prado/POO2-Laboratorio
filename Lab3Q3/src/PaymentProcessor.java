851176475bd2989029b27a8ab094d880e6d19c7f
CarlosL <carloseduardocostaprado@gmail.com>
2024-09-12 08:02:49 -0300

    Lab3v2

---
+++ Lab3Q3/src/PaymentProcessor.java
@@ -0,0 +1,5 @@
+// interface/PaymentProcessor.java
+
+public interface PaymentProcessor {
+    void processPayment(double amount, String recipient);
+}
---
+++ Lab3Q3/src/StripeService.java
@@ -0,0 +1,8 @@
+// services/StripeService.java
+
+public class StripeService {
+    public void charge(double amount, String recipient) {
+        // Lógica para processar pagamento com Stripe
+        System.out.println("Processando pagamento de $" + amount + " para " + recipient + " via Stripe.");
+    }
+}
